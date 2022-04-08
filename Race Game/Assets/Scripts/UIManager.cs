ebc4ece0f1d47992852f9d75ae2fc257d16118cf
DJ.Knight <djknight90@outlook.com>
2022-04-08 12:10:27 +1000

    Unity update to 2020.3.31f1 from 2020.3.30f1

--- Race Game/Assets/Scripts/UIManager.cs
+++ Race Game/Assets/Scripts/UIManager.cs
@@ -8,7 +8,8 @@ public class UIManager : MonoBehaviour
     public static UIManager instance;
 
     public TMP_Text lapCounterText, bestLapTimeText, currentLapTimeText;
-    private void Awake() {
+    private void Awake()
+    {
         instance = this;
     }
 
@@ -16,12 +17,12 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
